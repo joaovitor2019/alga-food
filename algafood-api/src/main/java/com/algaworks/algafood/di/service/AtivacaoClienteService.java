ed927826222a2de63ec8058c005e3bad87b9f851
joaovitor2019 <vitor.alves99@yahoo.com>
2024-09-23 13:59:52 -0300

    resolvendo ambiguidade com lista de beans

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -5,16 +5,20 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
-    private Notificador notificador;
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-    }
+        for (Notificador notificador : notificadores) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        }
 
+    }
 }
