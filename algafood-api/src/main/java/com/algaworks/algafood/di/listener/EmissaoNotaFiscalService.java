f9120e07be9ca56dd1c8e5645e1d60fe2bea397f
joaovitor2019 <vitor.alves99@yahoo.com>
2024-09-23 15:15:03 -0300

    Publicando e consumindo eventos customizados

---
+++ algafood-api/src/main/java/com/algaworks/algafood/di/listener/EmissaoNotaFiscalService.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.di.listener;
+
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmissaoNotaFiscalService {
+
+
+
+    @EventListener
+    public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+        System.out.println("Emitindo Nota Fiscal para cliente "+ event.getCliente().getNome());
+    }
+}
--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,42 +1,19 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+@Component
+public class AtivacaoClienteService {
 
-//@Component
-public class AtivacaoClienteService /*implements InitializingBean, DisposableBean*/ {
-
-    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
-    private Notificador notificador;
-
-    //@PostConstruct
-    public void init() {
-        System.out.println("Init " + notificador);
-    }
-
-    //@PreDestroy
-    public void destroy() {
-        System.out.println("Destroy");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
     }
-
-    /*@Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Init " + notificador);
-    }*/
 }
